df1579e6a8da4952c2c103efe17767348a5ae3e4
heam666 <41954655+heam666@users.noreply.github.com>
2020-08-20 21:41:06 +0530

    Add files via upload

---
+++ problem16.java
@@ -0,0 +1,31 @@
+/*
+ Sorting a stack using a temporary stack
+ */
+import java.util.Stack;
+
+public class problem16 {
+	public Stack<Integer> sorting(Stack<Integer> original) {
+		Stack<Integer> temporaryStack = new Stack<>();
+		while(!original.isEmpty()) {
+			int x = original.pop();
+			while(!temporaryStack.isEmpty() && temporaryStack.peek() > x) {
+				original.push(temporaryStack.pop());
+				}
+			temporaryStack.push(x);
+			}
+		return temporaryStack;
+		}
+	public static void main(String args[]) {
+		problem16 s = new problem16();
+	    Stack<Integer> originalStack = new Stack<>();
+	    originalStack.add(14);
+	    originalStack.add(9);
+	    originalStack.add(67);
+	    originalStack.add(91);
+	    originalStack.add(101);
+	    originalStack.add(25);
+	    System.out.println("Original Stack: " + originalStack);
+	    Stack<Integer> sortedStack= s.sorting(originalStack);
+	    System.out.println("Sorted Stack is:" + sortedStack);
+	    }
+	}
